226a7cfa6b957dc8ab14294a25ceb319580d98a5
xiejon <jxie77@gmail.com>
2023-08-10 15:30:13 -0400

    Add sample channel data & interface

---
+++ src/data/channels.ts
@@ -0,0 +1,12 @@
+import { Channel } from '../interfaces/channel';
+
+export const channels: Channel[] = [
+  {
+    id: "1",
+    name: "General",
+  },
+  {
+    id: "2",
+    name: "SF Bay Climbers",
+  },
+];
